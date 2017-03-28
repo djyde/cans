58954ae24b10067816865938e5c25eab6a1ad92c
Randy <randypriv@gmail.com>
2017-03-28 17:48:08 +0800

    add high order observable test

--- test/observer.test.js
+++ test/observer.test.js
@@ -6,58 +6,116 @@ import { mount, shallow } from 'enzyme'
 
 describe('Observer', () => {
 
-  const app = cans()
+  describe('normal observable', () => {
+    const app = cans()
 
-  const counterModel = {
-    namespace: 'counter',
-    observable: observable({
-      // state
-      count: 0,
+    const counterModel = {
+      namespace: 'counter',
+      observable: observable({
+        // state
+        count: 0,
 
-      // action
-      incr: action.bound(function () {
-        this.count += 1
-      }),
-      decr: action.bound(function () {
-        this.count -= 1
+        // action
+        incr: action.bound(function () {
+          this.count += 1
+        }),
+        decr: action.bound(function () {
+          this.count -= 1
+        })
       })
+    }
+
+    app.model(counterModel)
+
+    const Counter = inject(({ models }) => {
+      return (
+        <div>
+          <span id='count'>{models.counter.count}</span>
+          <Toolbar title='test' />
+        </div>
+      )
     })
-  }
 
-  app.model(counterModel)
+    const Toolbar = inject(({ models, title }) => {
+      return (
+        <div>
+          <div id='title'>{title}</div>
+          <button onClick={models.counter.incr}>+</button>
+          <button onClick={models.counter.decr}>-</button>
+        </div>
+      )
+    })
 
-  const Counter = inject(({ models }) => {
-    return (
-      <div>
-        <span id='count'>{models.counter.count}</span>
-        <Toolbar title='test' />
-      </div>
-    )
+    const wrapped = shallow(<Counter.wrappedComponent models={{ counter: app.models.counter }} />)
+
+    it('should increase count', done => {
+      app.models.counter.incr()
+      assert.equal(wrapped.find('#count').first().text(), '1')
+      done()
+    })
+
+    it('should decre count', done => {
+      app.models.counter.decr()
+      app.models.counter.decr()
+      assert.equal(wrapped.find('#count').first().text(), '-1')
+      done()
+    })
   })
 
-  const Toolbar = inject(({ models, title }) => {
-    return (
-      <div>
-        <div id='title'>{title}</div>
-        <button onClick={models.counter.incr}>+</button>
-        <button onClick={models.counter.decr}>-</button>
-      </div>
-    )
-  })
+  describe('high order observable', () => {
+    const app = cans()
 
-  const wrapped = shallow(<Counter.wrappedComponent models={{ counter: counterModel.observable }} />)
+    const counterModel = {
+      namespace: 'counter',
+      observable: app => observable({
+        // state
+        count: 0,
 
-  it('should increase count', done => {
-    counterModel.observable.incr()
-    assert.equal(wrapped.find('#count').first().text(), '1')
-    done()
-  })
+        // action
+        incr: action.bound(function () {
+          this.count += 1
+        }),
+        decr: action.bound(function () {
+          this.count -= 1
+        })
+      })
+    }
 
-  it('should decre count', done => {
-    counterModel.observable.decr()
-    counterModel.observable.decr()
-    assert.equal(wrapped.find('#count').first().text(), '-1')
-    done()    
+    app.model(counterModel)
+
+    const Counter = inject(({ models }) => {
+      return (
+        <div>
+          <span id='count'>{models.counter.count}</span>
+          <Toolbar title='test' />
+        </div>
+      )
+    })
+
+    const Toolbar = inject(({ models, title }) => {
+      return (
+        <div>
+          <div id='title'>{title}</div>
+          <button onClick={models.counter.incr}>+</button>
+          <button onClick={models.counter.decr}>-</button>
+        </div>
+      )
+    })
+
+    const wrapped = shallow(<Counter.wrappedComponent models={{ counter: app.models.counter }} />)
+
+    it('should increase count', done => {
+      app.models.counter.incr()
+      assert.equal(wrapped.find('#count').first().text(), '1')
+      done()
+    })
+
+    it('should decre count', done => {
+      app.models.counter.decr()
+      app.models.counter.decr()
+      assert.equal(wrapped.find('#count').first().text(), '-1')
+      done()
+    })
   })
 
 })
